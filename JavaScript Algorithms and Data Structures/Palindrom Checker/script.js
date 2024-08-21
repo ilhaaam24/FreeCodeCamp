1dc1f443694e496409ed7fd3e34b369a0ef2a90c
Ilham <mi1797128@gmail.com>
2024-08-21 10:32:56 +0700

    add logic reasult

--- JavaScript Algorithms and Data Structures/Palindrom Checker/script.js
+++ JavaScript Algorithms and Data Structures/Palindrom Checker/script.js
@@ -1,10 +1,18 @@
 const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+const p = document.createElement("p");
 
 
 checkBtn.addEventListener("click", function() {
+    event.preventDefault(); //mencegah reload halaman
   const input = document.getElementById("text-input").value;
+  const result = document.getElementById("result");
 
   if(input === "") {
     alert("Please input a value");
+  }
+  if(input.length === 1) {
+    p.innerText = `${input} is a palindrome`;
+    result.appendChild(p);
   }
 })
